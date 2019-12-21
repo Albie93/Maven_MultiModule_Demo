24b207882ec9e831ffad43463da2a0110567bfc8
Albie93 <albiestar779@gmail.com>
2019-12-22 01:08:40 +0530

    ïnit

---
+++ from-Intelliji/src/main/java/com/intelliji/Demo2.java
@@ -0,0 +1,15 @@
+package com.intelliji;
+
+import java.sql.SQLOutput;
+
+public class Demo2 {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Inside Module 2- Created By Intelliji");
+
+    }
+
+}
---
+++ jpa-entities/src/main/java/Demo1.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class Demo1 {
+
+    @Test
+    public void Test1(){
+
+
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void Test2(){
+
+        System.out.println("Test2");
+
+    }
+
+}
